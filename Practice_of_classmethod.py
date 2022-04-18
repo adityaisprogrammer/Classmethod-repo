7c73edf9cc03a04709407b7fe54874ff22999b29
Aditya <ajit.alkem79@gmail.com>
2022-04-18 19:15:43 +0530

    modified

--- Practice_of_classmethod.py
+++ Practice_of_classmethod.py
@@ -1,3 +1,32 @@
+#sample program
+
+#class Employee:
+  #  no_of_leaves = 9
+
+ #   def __init__(self, name, salary, role):
+ #       self.name = name 
+   #     self.salary = salary
+  #      self.role = role
+ #   @classmethod
+ #   def from_str(cls, string):
+    #params = string.split("-")
+    #print(params)
+    #return cls(params[0], params[1], params[2])
+ #    return cls(*string.split("-"))
+#
+#    @classmethod
+#    def change_leaves(cls, newleaves):
+#        cls.no_of_leaves = newleaves 
+#        cls.no_of_leaves = newleaves 
+
+#aditya = Employee.from_str("Aditya-675634-Programmer")
+
+#print(aditya.no_of_leaves)
+#aditya.change_leaves(34)
+#print(aditya.no_of_leaves)
+
+# Experiment program
+
 class Employee:
     no_of_leaves = 9
 
@@ -5,19 +34,16 @@ class Employee:
         self.name = name 
         self.salary = salary
         self.role = role
-    @classmethod
-    def from_str(cls, string):
-    #params = string.split("-")
-    #print(params)
-    #return cls(params[0], params[1], params[2])
-     return cls(*string.split("-"))
 
-    @classmethod
-    def change_leaves(cls, newleaves):
-        cls.no_of_leaves = newleaves 
 
-aditya = Employee.from_str("Aditya-675634-Programmer")
+aditya = Employee("Aditya" , 675634 , "Programmer")        
 
 print(aditya.no_of_leaves)
-aditya.change_leaves(34)
+print(aditya.__dict__)
+Employee.no_of_leaves = 38
 print(aditya.no_of_leaves)
+print(aditya.__dict__)
+aditya.no_of_leaves = 100
+print(aditya.no_of_leaves)
+print(aditya.__dict__)
+print(Employee.no_of_leaves)
